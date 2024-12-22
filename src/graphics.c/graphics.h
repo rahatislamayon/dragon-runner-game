19cb780141a180cea59cb1003adedac59752572e
rahat islam ayon <rahatislamayon@gmail.com>
2024-12-22 14:31:11 +0600

    Add files via upload

---
+++ src/graphics.c/graphics.h
@@ -0,0 +1,9 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+void drawDragon(int position);
+void drawObstacle(int position);
+void drawGround(int groundLevel);
+void clearScreen();
+
+#endif // GRAPHICS_H
